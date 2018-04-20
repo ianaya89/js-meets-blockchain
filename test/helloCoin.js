7bbf6d8f8e8e1fd5c5079c2483d3f0c79d1c2f86
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-04-20 00:31:08 -0300

    Improve test

--- test/helloCoin.js
+++ test/helloCoin.js
@@ -1,12 +1,9 @@
 const HelloCoin = artifacts.require('./HelloCoin.sol')
 
 contract('HelloCoin', function(accounts) {
-  it('should put 1000 HelloCoin in the first account', function() {
-    return HelloCoin.deployed().then(function(instance) {
-      return instance.mint.call(accounts[0], 1000)
-        .then(function (balance) {
-          assert.equal(balance.toNumber(), '1000', '')
-        })
-    })
+  it('should put 1000 HelloCoin in the first account', () => {
+    return HelloCoin.deployed()
+      .then(instance => instance.mint.call(accounts[0], 1000))
+      .then(balance => assert.equal(balance.toNumber(), '1000'))
   })
 })
